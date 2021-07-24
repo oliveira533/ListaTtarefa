2e99cd5344bcd0b9c719e23af4a9c62836ef37b2
Gustavo de Oliveira <go609127@gmail.com>
2021-07-24 20:09:04 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,12 +8,12 @@ function fnAdiciona(){
         
     }
     if (tTarefa != null){
-        let oItem = document.createElement('LI');
-        let tTexto = document.createTextNode(tTarefa);
-        oItem.appendChild(tTexto);
+        let oItem = document.createElement('LI'); // criando o elemento da lista
+        let tTexto = document.createTextNode(tTarefa); // criando o texto do elemento
+        oItem.appendChild(tTexto); // colocando texto no elemennto
         
 
-        var lLista = document.getElementById('tarefas');
-        lLista.insertBefore(oItem, lLista.childNodes[nCont++]);
+        var lLista = document.getElementById('tarefas'); // puxando a lista
+        lLista.insertBefore(oItem, lLista.childNodes[nCont++]); // inserindo elemento na lista
     }
 }
