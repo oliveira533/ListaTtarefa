3b7385aef12ea9fb1a9e3a7be06cf8b738751433
Gustavo de Oliveira <go609127@gmail.com>
2021-07-24 18:10:51 -0300

    script
    iniciei teste básicos

---
+++ script.js
@@ -0,0 +1,8 @@
+function fnAdiciona(){
+    var tTarefa = prompt('Infrome uma tarefa'); // pega a tarefa
+
+    while( tTarefa == '' || tTarefa == ' '){ // checa se o nome da tarefa está vazio
+        tTarefa = prompt('Infrome uma tarefa');
+        
+    }
+}
